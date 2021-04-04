5cc12966d8483d0b8ccfec17f1cf3f22f69de79e
Cooper <nickpartum@gmail.com>
2021-04-04 17:29:07 +0300

    Add app components

---
+++ src/utils/validationRules.ts
@@ -0,0 +1,33 @@
+// Finish
+export const isValidAge = (age: number) => (age >= 21)
+export const isValidExperience = (age: number, experience: number) => ((experience >= 0) && (experience < age))
+export const isValidYearlyIncome = (yearlyIncome: number) => ((yearlyIncome >= 0) && (yearlyIncome <= 1e6))
+export const isValidExpirationDate = (expirationDate: string) => {
+    const reg1 = /^\d{4}-\d{2}-\d{2}$/  // YYYY-MM-DD
+    const reg2 = /^\d{2}\/\d{2}\/\d{4}$/  // MM/DD/YYYY
+    // Has wrong format
+    if (!reg1.test(expirationDate) && !reg2.test(expirationDate)) {
+        return false
+    }
+    const inputDate = new Date(expirationDate)
+    const curentDate = new Date()
+    // Is bigger than current date
+    return inputDate > curentDate
+}
+export const isValidPhone = (phone: string) => {
+    const regexp = /(^\d{10}$)|(^[1]{1}\d{10}$)|(^[+][1]{1}\d{10}$)/
+    return regexp.test(phone)
+}
+export const isValidEmail = (email: string) => {
+    const regexp = /^[\w-.]+@[\w-]+.+[\w-]{2,4}$/
+    return regexp.test(email)
+}
+export const isValidHasChildren = (hasChildren: string) => {
+    return ((hasChildren === 'TRUE') || (hasChildren === 'FALSE') || (hasChildren === ''))
+}
+export const isValidLicenseNumber = (licenseNumber: string) => {
+    // Must have 6 digits or letters
+    const regexp = /^\w{6}$/
+    return regexp.test(licenseNumber);
+}
+export const isEmptyValue = (value: string) => (value === '')
